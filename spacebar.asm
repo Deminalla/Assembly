f8d5276e32cb659e4a1388726463696a6c15aa52
Deniza <65849358+Deminalla@users.noreply.github.com>
2021-10-27 13:56:31 +0300

    Update spacebar.asm

--- spacebar.asm
+++ spacebar.asm
@@ -1,84 +1,83 @@
-;Parašykite programą, kuri atspausdina tarpų pozicijas įvestoje simbolių eilutėje;  Pvz.: įvedus asl A1p3i DI iiO turi atspausdinti 4 10 13
 ;Write a program that would print out the positions of spacebars in a string that the user input. For example, input asl A1p3i DI iiO would print 4 10 13
-.MODEL small	;atminties modelis
-.STACK 100h		;stack'o dydis
-.DATA		;aprasom duomenu segmenta
-buff db  100            ;kiek max simboliu galima ivesti, nusprendziau, kad 100
-     db  ?              ;kiek pats naudotojas iveda simboliu, tai ?, nes kol kas neaisku ir pati programa paskui suskaiciuos
-     db  100 dup(0)     ;dup yra tai kartojasi, pvz 100dup(0), tai 100 kartu bus 0, beje čia yra išsaugomi visi įvesti simboliai (eilutė visada baigiasi su 13, nes tai yra enter?)   
-new_line db  0dh, 0ah, '$'  ;cia tiesiog tuscia, galima paskui panaudot kaip ednl arba \n
-array db 100 dup(0)     ;padariau masyva, išskiria duomenų segmente vietą 100 baitams, kurių visos reikšmės 0        
-.CODE		;kodo segmentas
+.MODEL small	;memory model
+.STACK 100h		;stack size
+.DATA		;data segment
+buff db  100            ;the maximum amount of symbols we can input
+     db  ?              ;how many symbols the user decided to enter, so ? because we don't know early on and the computer will count it itself later
+     db  100 dup(0)     ;dup (duplicate) so 100dup(0) would be 100 times 0, this part also saves our inputs (it will end with 13 because enter)   
+new_line db  0dh, 0ah, '$'  ;empty, we can use it as \n or andl
+array db 100 dup(0)     ;the array saves 100 bytes of space which all are 0 for now       
+.CODE		;code segment
 strt:
-mov ax,@data	;ds registro inicializavimas    
-mov ds,ax               ;sita komandų pora į registrą DS perkelia duomenų segmento pradžios paragrafo numerį.
+mov ax,@data	;ds register initialization  
+mov ds,ax               ;this command moves the DS the beginning of the paragraph's number to DS 
 
-mov ah, 0Ah             ;ivesti string su dx
-mov dx, offset buff     ;tikrinam, kiek galim ivesti
+mov ah, 0Ah             ;input string with dx
+mov dx, offset buff     ;let's check the limit of how much we can input
 int 21h   
-mov si, offset buff + 1 ;si registrui priskiriu ivestu simboliu adresa
-mov cl, [si]            ;cl = ivestu simboliu sk  
-mov dl, 0               ;jis pradzioje kazkoks kitoks sk, tai neutralizuojam
+mov si, offset buff + 1 ;si = address of input symbols
+mov cl, [si]            ;cl = amount of input symbols  
+mov dl, 0               ;we will use dl later for smth else, so we neutralize it after the string input
  
-testing:                ;tikrinam kiekviena ivesta zenkla
-inc dl                  ;nustatom nariu pozicija    
-inc si                  ;kad nuskaitytu ten, kur 100dup(0), o ne kur ?, cia tas pats kaip byte ptr [si] + 1
-mov al, byte ptr [si]   ;al - priskiriu si-elinta (kazkelinta) simboli
-cmp al, 20h             ;tarpas prilygsta 20 16-taineje sistemoje, galima rasyti ir ' ' cia butu char formatu tarpas
-je tarpas               ;jump if equal
-cmp dl, cl              ;ziurim ar visus simbolius patikrinom
+testing:                ;check every single symbol
+inc dl                  ;get the positions of the symbols    
+inc si                  ;we need it to read from this part: 100dup(0) and not ?, inc si = byte ptr [si] + 1
+mov al, byte ptr [si]   ;al = si th (n-th) element
+cmp al, 20h             ;spacebar = 20h, 20h = ' ' which would be in char format instead of hex 
+je spacebar             ;jump if equal
+cmp dl, cl              ;check if we compared and analysed all the symbols 
 jb testing              ;jump if its below/less than cl
-jmp line                ;jeigu perziurejom visus simbolius, einam prie isvedinejimo
+jmp line                ;if we checked all symbols we can end this cycle
 
-tarpas:
-inc bx                 ;bx - tarpu sk, kad zinot kiek kartu paskui isvesti
-mov di, offset array   ;di - masyvo adresas    
-mov [di+bx], dl        ;i masyva idedam tarpo pozicija
+spacebar:
+inc bx                 ;bx - amount of spacebars, so we know how may positions we need to output
+mov di, offset array   ;di - address of the array   
+mov [di+bx], dl        ;put the position of the spacebar into the array
 cmp dl, cl
-jb testing             ;griztam prie simboliu tikrinimo
+jb testing             ;continue checking other symbols
 
 line:
 mov ah, 9                ;string output
-mov dx, offset new_line  ;čia rodo nuo kur pradėti skaityti, $ ženklas new_line gale rodo kada sustoti
+mov dx, offset new_line  ;this shows from where to start scanning, $ at the end of new_line shows where to stop
 int 21h 
 
-skaitmenu_sk: 
+digit_amount: 
 mov ah, 2         
-mov dl, 20h     ;output spacebar
+mov dl, 20h     ;output spacebar 
 int 21h         
 inc di              
-mov dl, [di]    ;dl'ui priskiriam kazkelinta masyvo elementa    
+mov dl, [di]    ;dl = di th array element   
 cmp dx, 9       
-ja dvizenklis   ;jeigu daugiau uz 9, tai bus dvizenklis
+ja 2digits      ;if it's more than 9, it has 2 digits
 
-add dx, 48      ;tokiu budu is decimal isves char, pagal ascii lentele ir bus teisingi nuo 0 iki 9
-mov ah, 2       ;output tarpo pozicija dl
+add dx, 48      ;all of this is in char format so we find where the decimals are in char 
+mov ah, 2       ;dl outputs the spacebar positions
 int 21h 
 sub bl, 1      
-cmp bl, 0       ;kad suktume ciklu tiek, kiek yra tarpu
-jne skaitmenu_sk
+cmp bl, 0       ;we spin the cycle until there are no more spacebar positions left
+jne digit_amount
 jmp pabaiga
 
-dvizenklis: 
-mov ax, dx    ;kadangi dvizenklis, viskas tilps i ax 
-mov dx, 0     ;viskas telpa i ax, tai dx=0
+2digits: 
+mov ax, dx    ;since it has only 2 digits, it will all fit into ax 
+mov dx, 0     ;since its all in, we need to make dx=0
 mov cx, 10     
-div cx        ;, dalyba is 10, ax pirmas sk, dx - antras sk (liekana)  
-push dx       ;liekana deti i stack, kad jo nepamest
-mov dx, ax    ;dx=ax, nes mum reikes isvesti dl tai pirma skaitemi jam prilyginam
+div cx        ;dicide cx/10, ax = 1st digit, dx = 2nd digit (leftover)  
+push dx       ;put the 2nd digit into stack so we don't lose it 
+mov dx, ax    ;dx=ax, because we output with dl 
 add dx, 48  
 mov ah, 2
 int 21h
 
-pop dx        ;isimam liekana is stack 
-add dx, 48    ;toliau tas pats kaip ir pries tai
+pop dx        ;get the 2nd digit out of stack 
+add dx, 48    
 mov ah, 2
 int 21h   
 sub bl, 1
 cmp bl, 0
-jne skaitmenu_sk
+jne digit_amount
 
 pabaiga:
-mov ax, 4C00h   ;programos darbo pabaiga
+mov ax, 4C00h   ;the end of the program
 int 21h
 end strt
