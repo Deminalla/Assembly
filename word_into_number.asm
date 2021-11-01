3b1552ce4416f62a13291066d4d4aeca9328676c
Deniza <65849358+Deminalla@users.noreply.github.com>
2021-11-01 18:55:05 +0300

    Update word_into_number.asm

--- word_into_number.asm
+++ word_into_number.asm
@@ -1,14 +1,16 @@
 .model small       
 .stack 100h
-.data  
-        
-myfile db  "duom.txt", 0      ;i picked the name myfile, 0 shows the end of the filename string, the txt file is in emu8086/MyBuild 
-results db "rez.txt", 0       ;my second file 
-read_buff db 100 dup (?)      ;this will help us read the file character by character
-input_descriptor dw ?          ;its a number that helps identify the file its working with   
+.data    
+
+read_buff db 10 dup (?)     ;this will help us read the file character by 10 characters    
+;position dw 0               ;so we know in which part of the 10 element buffer we are in
+;full dw 0                   ;to check how many symbols we have to read in 10 element buffer or if its the end yet        
+myfile db  "duom.txt", 0    ;i picked the name myfile, 0 shows the end of the filename string, the txt file is in emu8086/MyBuild 
+results db "rez.txt", 0     ;my second file 
+input_descriptor dw ?       ;its a number that helps identify the file its working with   
 output_descriptor dw ?
 help db "Si programa jusu faile parasyto teksto skaicius pavers zodiais$" ;if nothing is given or it only gets /? 
-err db "Ivyko klaida$" ;literally any type of error
+smth_wrong db "Ivyko klaida$" ;literally any type of error
 
 nr_zero db "nulis$"
 nr_one db "vienas$"
@@ -24,9 +26,9 @@ nr_nine db "devyni$"
 strt:
 mov ax,@data
 mov ds, ax
-  
-call open_file    
-call create_file
+
+call open_file    ;unidentified open_file, bruh 
+call create_file  ;unidentified create_file, bruh
 
 reading:
 mov ah, 3Fh    ;this will read the 1st file  
@@ -35,123 +37,25 @@ mov cx, 1      ;so it will read only 1 byte
 mov dx, offset read_buff ;this will save that one element in that array for now
 int 21h  
 
-cmp ax, 0      ;if there's nothing left to read, end of file
-je finish
-
-mov si, dx
-cmp [si], '9'
-jbe is_it_a_number
-jmp non_number
-
-is_it_a_number:
-cmp [si], '0' 
-je zero
-cmp [si], '1'
-je one
-cmp [si], '2'
-je two
-cmp [si], '3' 
-je three
-cmp [si], '4' 
-je four
-cmp [si], '5'
-je five
-cmp [si], '6'
-je six
-cmp [si], '7'
-je seven
-cmp [si], '8'
-je eight
-cmp [si], '9'
-je nine 
-
-non_number:
-mov ah, 40h
-mov bx, output_descriptor
-mov cx, 1  ;STRING LENGTH.
-mov dx, offset read_buff
-int  21h  
+cmp ax, 0  ;if there's nothing left to read, end of file
+je close_files  
+call write_file
 jmp reading
 
-zero:
-mov ah, 40h
-mov bx, output_descriptor
-mov cx, 5  ;STRING LENGTH.
-mov dx, offset nr_zero
-int  21h 
-jmp reading 
+close_files: 
+mov ah, 3Eh
+mov bx, input_descriptor
+int 21h
 
-one:
-mov ah, 40h
+mov ah, 3Eh
 mov bx, output_descriptor
-mov cx, 6  ;STRING LENGTH.
-mov dx, offset nr_one
-int  21h      
-jmp reading
+int 21h  
+jmp finish
 
-two:
-mov ah, 40h
-mov bx, output_descriptor
-mov cx, 2  ;STRING LENGTH.
-mov dx, offset nr_two
-int  21h  
-jmp reading
-
-three:
-mov ah, 40h
-mov bx, output_descriptor
-mov cx, 4  ;STRING LENGTH.
-mov dx, offset nr_three
-int  21h 
-jmp reading
-
-four:
-mov ah, 40h
-mov bx, output_descriptor
-mov cx, 6  ;STRING LENGTH.
-mov dx, offset nr_four
-int  21h  
-jmp reading
-
-five:
-mov ah, 40h
-mov bx, output_descriptor
-mov cx, 5  ;STRING LENGTH.
-mov dx, offset nr_five
-int  21h   
-jmp reading
-
-six:
-mov ah, 40h
-mov bx, output_descriptor
-mov cx, 4  ;STRING LENGTH.
-mov dx, offset nr_six
-int  21h   
-jmp reading       
-
-seven:
-mov ah, 40h
-mov bx, output_descriptor
-mov cx, 7  ;STRING LENGTH.
-mov dx, offset nr_seven
-int  21h   
-jmp reading
-
-eight:
-mov ah, 40h
-mov bx, output_descriptor
-mov cx, 7  ;STRING LENGTH.
-mov dx, offset nr_eight
-int  21h 
-jmp reading
-
-nine:
-mov ah, 40h
-mov bx, output_descriptor
-mov cx, 6  ;STRING LENGTH.
-mov dx, offset nr_nine
-int  21h 
-jmp reading 
+error:
+mov ah, 9
+mov dx, offset help ;should error/help messages be stdout or in the rez file???
+int 21h 
 
 proc open_file
 mov ah, 3Dh    ;it means we will open a file 
@@ -172,27 +76,96 @@ mov output_descriptor, ax
 ret
 endp create_file 
 
-error:
-mov ah, 9
-mov dx, offset help ;should error/help messages be stdout or in the rez file???
-int 21h 
-
-finish: 
-mov ah, 3Eh
-mov bx, input_descriptor
-int 21h
-
-mov ah, 3Eh
+proc write_file
+mov si, dx
+mov ah, 40h
 mov bx, output_descriptor
-int 21h
+cmp byte ptr[si], '0'  
+je zero
+cmp byte ptr[si], '1'
+je one
+cmp byte ptr[si], '2'
+je two
+cmp byte ptr[si], '3' 
+je three
+cmp byte ptr[si], '4'    ;argument needs type override
+je four
+cmp byte ptr[si], '5'
+je five                  
+cmp byte ptr[si], '6'
+je six        
+cmp byte ptr[si], '7'
+je seven
+cmp byte ptr[si], '8'
+je eight
+cmp byte ptr[si], '9'
+je nine 
+;not a number
+mov cx, 1  ;STRING LENGTH.
+mov dx, offset read_buff
+int  21h  
+ret
+zero:
+mov cx, 5  ;STRING LENGTH.
+mov dx, offset nr_zero
+int  21h 
+ret 
+one:
+mov cx, 6  ;STRING LENGTH.
+mov dx, offset nr_one
+int  21h      
+ret
+two:
+mov cx, 2  ;STRING LENGTH.
+mov dx, offset nr_two
+int  21h  
+ret
+three:
+mov cx, 4  ;STRING LENGTH.
+mov dx, offset nr_three
+int  21h 
+ret
+four:
+mov cx, 6  ;STRING LENGTH.
+mov dx, offset nr_four
+int  21h  
+ret
+five:
+mov cx, 5  ;STRING LENGTH.
+mov dx, offset nr_five
+int  21h   
+ret
+six:
+mov cx, 4  ;STRING LENGTH.
+mov dx, offset nr_six
+int  21h   
+ret       
+seven:
+mov cx, 7  ;STRING LENGTH.
+mov dx, offset nr_seven
+int  21h   
+ret
+eight:
+mov cx, 7  ;STRING LENGTH.
+mov dx, offset nr_eight
+int  21h 
+ret
+nine:
+mov cx, 6  ;STRING LENGTH.
+mov dx, offset nr_nine
+int  21h 
+jmp reading
+ret
+endp write_file
 
+finish:
 mov ax, 4Ch		
 int 21h			
-end strt    
+end strt       
 
-;Visi parametrai programai turi buti paduodami komandine eilute, o ne prasant juos ivesti is klaviaturos. Pvz.: antra duom.txt rez.txt
+;Visi parametrai programai turi buti paduodami komandine eilute, o ne prasant juos ivesti is klaviaturos. Pvz.: antra duom.txt rez.txt  (antra cia mano asm failo pavadinimas)
 ;Jeigu programa paleista be parametru arba parametrai nekorektiski, reikia atspausdinti pagalbos pranesima toki pati, kaip paleidus programa su parametru /?.
 ;Programa turi apdoroti ivedimo isvedimo (ir kitokias) klaidas. Pvz, nustacius, kad nurodytas failas neegzistuoja - ji turi isvesti pagalbos pranesima ir baigti darba.
 ;Failu skaitymo ar rasymo buferio dydis turi buti nemazesnis uz 10 baitu.
 ;Failo dydis gali virsyti skaitymo ar rasymo buferio dydi.
-;Panaudot funkcija. (call?????)       
+;Panaudot funkcija. (call i think)       
