2e913111fcfec78ec98f964edff0d0a7145d90aa
Deniza <65849358+Deminalla@users.noreply.github.com>
2021-11-06 19:25:09 +0300

    Update word_into_number.asm

--- word_into_number.asm
+++ word_into_number.asm
@@ -2,15 +2,18 @@
 .stack 100h
 .data    
 
-read_buff db 10 dup (?)     ;this will help us read the file character by 10 characters    
-;position dw 0              ;so we know in which part of the 10 element buffer we are in
-;full dw 0                  ;to check how many symbols we have to read in 10 element buffer or if its the end yet        
+read_buff db 10 dup (?)     ;this will help us read the file character by 10 characters       
+symbols dw 0                ;to check how many symbols we have to read in 10 element buffer or if its the end yet 
+element dw ?
+
 myfile db  "duom.txt", 0    ;i picked the name myfile, 0 shows the end of the filename string, the txt file is in emu8086/MyBuild 
 results db "rez.txt", 0     ;my second file 
 input_descriptor dw ?       ;its a number that helps identify the file its working with   
-output_descriptor dw ?
+output_descriptor dw ? 
+
 help db "Si programa jusu .txt faile parasyto teksto skaicius pavers zodiais. Pvz, as12 bus asvienasdu$" ;if nothing is given or it only gets /? 
 smth_wrong db "Ivyko klaida$" ;literally any type of error
+new_line db  0dh, 0ah, '$'
 
 nr_zero db "nulis$"
 nr_one db "vienas$"
@@ -27,19 +30,21 @@ strt:
 mov ax,@data
 mov ds, ax
 
-call open_file    ;unidentified open_file, bruh 
-call create_file  ;unidentified create_file, bruh
+call open_file     
+call create_file  
 
 reading:       
 mov ah, 3Fh              ;this will read the 1st file  
 mov bx, input_descriptor ;so it knkows which file to read
-mov cx, 1                ;so it will read only 1 byte
-mov dx, offset read_buff ;this will save that one element in that array for now
+mov cx, 10                ;so it will read 10 bytes
+mov dx, offset read_buff ;this will save the elements in that array for now
 int 21h
 jc error          ;an error when trying to read the file  
 
-cmp ax, 0         ;if there's nothing left to read, end of file
+mov symbols, ax
+cmp symbols, 0         ;if there's nothing left to read, end of file
 je finish  
+mov si, dx 
 call write_file
 jmp reading
 
@@ -79,103 +84,146 @@ mov output_descriptor, ax
 ret
 endp create_file 
 
-proc write_file
-mov si, dx
-mov ah, 40h
-mov bx, output_descriptor 
-jc error   ;unable to write anything? is this necessary?
+proc write_file 
+ciklas:  
+jc error   ;unable to write anything, yep this is necessary
 cmp byte ptr[si], '0'  
 jne one 
+mov ah, 40h
+mov bx, output_descriptor
 mov cx, 5  ;STRING LENGTH.
 mov dx, offset nr_zero
 int  21h 
-ret   
+jmp middle   
 
 one:
 cmp byte ptr[si], '1'
-jne two 
+jne two  
+mov ah, 40h
+mov bx, output_descriptor
 mov cx, 6  ;STRING LENGTH.
 mov dx, offset nr_one
 int  21h      
-ret
+jmp middle
 
 two:
 cmp byte ptr[si], '2'
-jne three 
+jne three   
+mov ah, 40h
+mov bx, output_descriptor
 mov cx, 2  ;STRING LENGTH.
 mov dx, offset nr_two
 int  21h  
-ret 
+jmp middle 
 
 three:
 cmp byte ptr[si], '3' 
-jne four  
+jne four 
+mov ah, 40h
+mov bx, output_descriptor 
 mov cx, 4  ;STRING LENGTH.
 mov dx, offset nr_three
 int  21h 
-ret 
+jmp middle 
+
+continue2: ;continue1 and continue2 is necessary because otherwise the jumps are out of range
+jmp ciklas
 
 four:
 cmp byte ptr[si], '4'   
-jne five
+jne five  
+mov ah, 40h
+mov bx, output_descriptor
 mov cx, 6  ;STRING LENGTH.
 mov dx, offset nr_four
 int  21h  
-ret  
+jmp middle  
 
 five:
 cmp byte ptr[si], '5'
-jne six
+jne six  
+mov ah, 40h
+mov bx, output_descriptor
 mov cx, 5  ;STRING LENGTH.
 mov dx, offset nr_five
-int  21h   
-ret 
+int  21h    
+middle:
+jmp testing    
 
 six:                 
 cmp byte ptr[si], '6'
 jne seven
+mov ah, 40h
+mov bx, output_descriptor
 mov cx, 4  ;STRING LENGTH.
 mov dx, offset nr_six
 int  21h   
-ret 
+jmp testing    
 
 seven:       
 cmp byte ptr[si], '7'
-jne eight
+jne eight  
+mov ah, 40h
+mov bx, output_descriptor
 mov cx, 7  ;STRING LENGTH.
 mov dx, offset nr_seven
 int  21h   
-ret   
+jmp testing 
+
+continue1:  ;otherwise its too big of a jump
+jmp continue2 ;it doesnt goto ciklas otherwise for some reason  
 
 eight:
 cmp byte ptr[si], '8'
-jne nine  
+jne nine 
+mov ah, 40h
+mov bx, output_descriptor 
 mov cx, 7  ;STRING LENGTH.
 mov dx, offset nr_eight
 int  21h 
-ret
+jmp testing
 
 nine:
 cmp byte ptr[si], '9'
-jne non_number  
+jne end_of_line 
+mov ah, 40h
+mov bx, output_descriptor 
 mov cx, 6  ;STRING LENGTH.
 mov dx, offset nr_nine
 int  21h 
-jmp reading
-ret
+jmp testing
+
+end_of_line:
+cmp byte ptr[si], 13  ;0Dh after 13 always comes 10 as and end of line and new line kind of thing
+jne non_number
+mov ah, 40h
+mov bx, output_descriptor
+mov cx, 1  ;STRING LENGTH.
+mov dx, offset new_line
+int  21h 
+jmp testing 
 
 non_number:
+cmp byte ptr[si], 10 ;jeigu ne 10 ir ne 13, tai jau tikrai kitoks simbolis 
+je testing  
+mov ax, [si]  
+mov element, ax
+mov ah, 40h
+mov bx, output_descriptor
 mov cx, 1  ;STRING LENGTH.
-mov dx, offset read_buff
+mov dx, offset element 
 int  21h  
-ret    
-endp write_file
-			
+
+testing:
+inc si   
+dec symbols  
+mov cx, symbols
+cmp cx, 0
+jne continue1 ;if its not 0 
+ret   
+endp write_file		
 end strt    
 
 ;Visi parametrai programai turi buti paduodami komandine eilute, o ne prasant juos ivesti is klaviaturos. Pvz.: antra duom.txt rez.txt  (antra cia mano asm failo pavadinimas)
 ;Jeigu programa paleista be parametru arba parametrai nekorektiski, reikia atspausdinti pagalbos pranesima toki pati, kaip paleidus programa su parametru /?.
 ;Programa turi apdoroti ivedimo isvedimo (ir kitokias) klaidas. Pvz, nustacius, kad nurodytas failas neegzistuoja - ji turi isvesti pagalbos pranesima ir baigti darba.
-;Failu skaitymo ar rasymo buferio dydis turi buti nemazesnis uz 10 baitu.
-;Failo dydis gali virsyti skaitymo ar rasymo buferio dydi.
-;Panaudot funkcija. (call i think)      
