52a1330a048e7b28c17fc0fec25cb9554a57024c
Deniza <65849358+Deminalla@users.noreply.github.com>
2021-11-07 14:05:20 +0300

    Update param_word_to_number.asm

--- param_word_to_number.asm
+++ param_word_to_number.asm
@@ -1,18 +1,20 @@
+;Jeigu programa paleista be parametru arba parametrai nekorektiski, reikia atspausdinti pagalbos pranesima toki pati, kaip paleidus programa su parametru /?.
+;Programa turi apdoroti ivedimo isvedimo (ir kitokias) klaidas. Pvz, nustacius, kad nurodytas failas neegzistuoja - ji turi isvesti pagalbos pranesima ir baigti darba. (AR man konkretizuot kokia klaida?)
 .model small       
 .stack 100h
 .data    
 
-read_buff db 10 dup (?)     ;this will help us read the file character by 10 characters       
-symbols dw 0                ;to check how many symbols we have to read in 10 element buffer or if its the end yet 
-element dw ?
+read_buff db 10 dup (?)       ;this will help us read the file by 10 characters       
+symbols dw 0                  ;to check how many symbols we have to read in a 10 element buffer or if its the end yet 
+element dw ?                  ;what character it is if not a digit
 
-myfile db 100 dup(0)        ;i picked the name myfile, 0 shows the end of the filename string, the txt file is in emu8086/MyBuild 
-results db 100 dup(0)       ;my second file 
+myfile db 100 dup(0)          ;1st file for reading
+results db 100 dup(0)         ;2nd file for writing
 
-input_descriptor dw ?       ;its a number that helps identify the file its working with   
+input_descriptor dw ?         ;descripto is a number that helps identify the file its working with, so it doesn't confuse it with other stuff 
 output_descriptor dw ?  
 
-sos db "Si programa jusu .txt faile parasyto teksto skaicius pavers zodiais. Pvz, as123 bus asvienasdutrys$" ;if nothing is given or it only gets /? 
+sos db "Programa jusu .txt faile parasyto teksto skaicius pavers zodiais. Pvz, as123 bus asvienasdutrys$" ;if gets /? or the parameters are incorrect
 smth_wrong db "Ivyko klaida$" ;literally any type of error
 new_line db  0dh, 0ah, '$'
 
@@ -22,22 +24,22 @@ nr_two db "du$"
 nr_three db "trys$"
 nr_four db "keturi$"
 nr_five db "penki$"
-nr_six db "sesi$"      ;fix the letters?  
+nr_six db "sesi$"       
 nr_seven db "septyni$" 
-nr_eight db "astuoni$" ;fix the letters?
+nr_eight db "astuoni$" 
 nr_nine db "devyni$"
 .code
 strt:    
 mov ax,@data
 mov ds, ax     
 
-mov bx, 82h ; adresuosime su bx
-mov si, offset myfile ;rasysime po simboli i vieta skirta duomenu failo vardui
+mov bx, 82h 
+mov si, offset myfile 
 
-cmp byte ptr es:[80h], 0 ;nepaduota parametru
+cmp byte ptr es:[80h], 0 ;no parameters were given
 je get_help
-cmp byte ptr es:[84h], 13 ;po parametro /? daugiau nieko ir nera
-je get_help
+cmp byte ptr es:[84h], 13 ;not enough parameters 
+je get_help 
 call duom_name  
 mov si, offset results 
 inc bx
@@ -47,29 +49,29 @@ call open_file
 call create_file  
 
 reading:      
-mov ah, 3Fh              ;this will read the 1st file  
-mov bx, input_descriptor ;so it knkows which file to read
-mov cx, 10                ;so it will read 10 bytes
-mov dx, offset read_buff ;this will save the elements in that array for now
+mov ah, 3Fh               ;read a file  
+mov bx, input_descriptor  ;so it knkows which file to read
+mov cx, 10                ;read 10 bytes
+mov dx, offset read_buff  ;save the elements in that array for now
 int 21h
-jc error          ;an error when trying to read the file  
+jc error            ;an error when trying to read the file  
 
-mov symbols, ax
-cmp symbols, 0         ;if there's nothing left to read, end of file
-je close_files  
-mov si, dx 
+mov symbols, ax     ;how many symbols we found out of 10
+cmp symbols, 0      ;if there's nothing left to read, end of file
+je close_files    
+mov si, dx          ;get the adress of read_buff
 call write_file
-jmp reading
+jmp reading         ;after checking 10 or less symbols, repeat
 
 get_help:
 mov ah, 9
-mov dx, offset sos
+mov dx, offset sos  ;save out sould :D
 int 21h
 jmp finish
 
 error:
 mov ah, 9
-mov dx, offset smth_wrong      ;for example 1st file duom,txt doesn't exist
+mov dx, offset smth_wrong    ;for example 1st file duom.txt doesn't exist
 int 21h 
 
 close_files:
@@ -84,12 +86,12 @@ finish:
 mov ax, 4Ch	  ;yep i wrote the finishing function in the middle :)	
 int 21h
 
-proc duom_name ;cia net neateina  
+proc duom_name  
 duom_file:
-cmp byte ptr es:[bx], 20h ;when its a space, that means afterwards comes rez.txt
+cmp byte ptr es:[bx], 20h ;when its a space, that means afterwards comes rez.txt so we end this
 je go_back
 mov dl, byte ptr es:[bx] 
-mov [si], dl  ;myfile
+mov [si], dl  ;fill in myfile 1 by 1 untill we get the whole name
 inc bx
 inc si
 jmp duom_file 
@@ -97,12 +99,12 @@ go_back:
 ret 
 endp duom_name
 
-proc rez_name ;cia net neateina  
+proc rez_name 
 rez_file:
-cmp byte ptr es:[bx], 13 ;when its a space, that means afterwards comes rez.txt
+cmp byte ptr es:[bx], 13 
 je get_back
 mov dl, byte ptr es:[bx] 
-mov [si], dl  ;myfile
+mov [si], dl  
 inc bx
 inc si
 jmp rez_file 
@@ -131,13 +133,13 @@ ret
 endp create_file 
 
 proc write_file 
-ciklas:  
-jc error   ;unable to write anything, yep this is necessary
-cmp byte ptr[si], '0'  
+cycle:  
+jc error   ;unable to write anything
+cmp byte ptr[si], '0'   
 jne one 
 mov ah, 40h
 mov bx, output_descriptor
-mov cx, 5  ;STRING LENGTH.
+mov cx, 5  ;lenght of characters to print
 mov dx, offset nr_zero
 int  21h 
 jmp middle   
@@ -147,7 +149,7 @@ cmp byte ptr[si], '1'
 jne two  
 mov ah, 40h
 mov bx, output_descriptor
-mov cx, 6  ;STRING LENGTH.
+mov cx, 6  ;lenght of characters to print
 mov dx, offset nr_one
 int  21h      
 jmp middle
@@ -157,7 +159,7 @@ cmp byte ptr[si], '2'
 jne three   
 mov ah, 40h
 mov bx, output_descriptor
-mov cx, 2  ;STRING LENGTH.
+mov cx, 2  ;lenght of characters to print
 mov dx, offset nr_two
 int  21h  
 jmp middle 
@@ -167,20 +169,20 @@ cmp byte ptr[si], '3'
 jne four 
 mov ah, 40h
 mov bx, output_descriptor 
-mov cx, 4  ;STRING LENGTH.
+mov cx, 4  
 mov dx, offset nr_three
 int  21h 
 jmp middle 
 
-continue2: ;continue1 and continue2 is necessary because otherwise the jumps are out of range
-jmp ciklas
+continue2: ;continue1 and continue2 is necessary, otherwise the jumps are out of range
+jmp cycle
 
 four:
 cmp byte ptr[si], '4'   
 jne five  
 mov ah, 40h
 mov bx, output_descriptor
-mov cx, 6  ;STRING LENGTH.
+mov cx, 6 
 mov dx, offset nr_four
 int  21h  
 jmp middle  
@@ -190,7 +192,7 @@ cmp byte ptr[si], '5'
 jne six  
 mov ah, 40h
 mov bx, output_descriptor
-mov cx, 5  ;STRING LENGTH.
+mov cx, 5  
 mov dx, offset nr_five
 int  21h    
 middle:
@@ -201,7 +203,7 @@ cmp byte ptr[si], '6'
 jne seven
 mov ah, 40h
 mov bx, output_descriptor
-mov cx, 4  ;STRING LENGTH.
+mov cx, 4  
 mov dx, offset nr_six
 int  21h   
 jmp testing    
@@ -211,20 +213,20 @@ cmp byte ptr[si], '7'
 jne eight  
 mov ah, 40h
 mov bx, output_descriptor
-mov cx, 7  ;STRING LENGTH.
+mov cx, 7  
 mov dx, offset nr_seven
 int  21h   
 jmp testing 
 
-continue1:  ;otherwise its too big of a jump
-jmp continue2 ;it doesnt goto ciklas otherwise for some reason  
+continue1:  
+jmp continue2  ;otherwise its too big of a jump
 
 eight:
 cmp byte ptr[si], '8'
 jne nine 
 mov ah, 40h
 mov bx, output_descriptor 
-mov cx, 7  ;STRING LENGTH.
+mov cx, 7  
 mov dx, offset nr_eight
 int  21h 
 jmp testing
@@ -234,29 +236,29 @@ cmp byte ptr[si], '9'
 jne end_of_line 
 mov ah, 40h
 mov bx, output_descriptor 
-mov cx, 6  ;STRING LENGTH.
+mov cx, 6  
 mov dx, offset nr_nine
 int  21h 
 jmp testing
 
 end_of_line:
-cmp byte ptr[si], 13  ;0Dh after 13 always comes 10 as and end of line and new line kind of thing
+cmp byte ptr[si], 13   ;0Dh after 13 always comes 10 as end of line and newline kind of thing so we do newline and ignore 10 later
 jne non_number
 mov ah, 40h
 mov bx, output_descriptor
-mov cx, 1  ;STRING LENGTH.
+mov cx, 1  
 mov dx, offset new_line
 int  21h 
 jmp testing 
 
 non_number:
-cmp byte ptr[si], 10 ;jeigu ne 10 ir ne 13, tai jau tikrai kitoks simbolis 
+cmp byte ptr[si], 10   ;ignore 10 because its a combo of 13 and 10 meaning newline
 je testing  
 mov ax, [si]  
 mov element, ax
 mov ah, 40h
 mov bx, output_descriptor
-mov cx, 1  ;STRING LENGTH.
+mov cx, 1  
 mov dx, offset element 
 int  21h  
 
@@ -265,10 +267,7 @@ inc si
 dec symbols  
 mov cx, symbols
 cmp cx, 0
-jne continue1 ;if its not 0 
+jne continue1 
 ret   
 endp write_file			
 end strt    
-
-;Jeigu programa paleista be parametru arba parametrai nekorektiski, reikia atspausdinti pagalbos pranesima toki pati, kaip paleidus programa su parametru /?.
-;Programa turi apdoroti ivedimo isvedimo (ir kitokias) klaidas. Pvz, nustacius, kad nurodytas failas neegzistuoja - ji turi isvesti pagalbos pranesima ir baigti darba.  
