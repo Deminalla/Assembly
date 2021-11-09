ff053b23fc2cec7bbf197679edd60e08f504fed2
Deniza <65849358+Deminalla@users.noreply.github.com>
2021-11-09 20:27:49 +0300

    Update param_word_to_number.asm

--- param_word_to_number.asm
+++ param_word_to_number.asm
@@ -1,19 +1,18 @@
-;
 .model small       
 .stack 100h
 .data    
 
-read_buff db 10 dup (?)       ;this will help us read the file by 10 characters       
-symbols dw 0                  ;to check how many symbols we have to read in a 10 element buffer or if its the end yet 
-element dw ?                  ;what character it is if not a digit
+read_buff db 10 dup (?)            
+symbols dw 0                  
+element dw ?                  
 
-myfile db 100 dup(0)          ;1st file for reading, I could make it a constant as  myfile db "duom.txt", 0
-results db 100 dup(0)         ;2nd file for writing
+myfile db 100 dup(0)          
+results db 100 dup(0)         
 
-input_descriptor dw ?         ;descripto is a number that helps identify the file its working with, so it doesn't confuse it with other stuff 
+input_descriptor dw ?          
 output_descriptor dw ?  
 
-sos db "Programa jusu .txt faile parasyto teksto skaicius pavers zodiais. Pvz, as123 bus asvienasdutrys$" ;if gets /? or the parameters are incorrect
+sos db "Programa jusu .txt faile parasyto teksto skaicius pavers zodiais. Pvz, as123 bus asvienasdutrys$" 
 read_wrong db "Nepavyko perskaityti failo$" 
 open_wrong db "Nepavyko atidaryti failo$" 
 create_wrong db "Nepavyko sukurti failo$"
@@ -30,53 +29,56 @@ nr_six db "sesi$"
 nr_seven db "septyni$" 
 nr_eight db "astuoni$" 
 nr_nine db "devyni$"
-.code
-strt:    
+.code    
 mov ax,@data
 mov ds, ax     
 
 mov bx, 82h 
-mov si, offset myfile 
+mov si, offset myfile
+mov di, offset results  
 
-cmp byte ptr es:[80h], 0  ;no parameters were given, this shows length of command line string, so if its mycode then 6?
+cmp byte ptr es:[80h], 0  
 je get_help
-cmp es:[82h], '?/' ;/? but it reads it in reverse
+cmp es:[82h], '?/' ;/?
 je get_help 
-call duom_name   
-mov bp, bx
-mov si, offset results 
-inc bx     
-cmp byte ptr es:[bp], 13  ;wehter its enter because if so, we are missing the 2nd parameter
-je get_help
-call rez_name
+call file_name   
 
 call open_file     
 call create_file  
 
 reading:      
-mov ah, 3Fh               ;read a file  
-mov bx, input_descriptor  ;so it knkows which file to read
-mov cx, 10                ;read 10 bytes
-mov dx, offset read_buff  ;save the elements in that array for now
+mov ah, 3Fh               
+mov bx, input_descriptor 
+mov cx, 10                
+mov dx, offset read_buff  
 int 21h
-jc error_read            ;an error when trying to read the file  
+jc error_read             
 
-mov symbols, ax     ;how many symbols we found out of 10
-cmp symbols, 0      ;if there's nothing left to read, end of file
+mov symbols, ax     ;kiek skaityti simboliu is 10  
+cmp symbols, 0      
 je close_files    
-mov si, dx          ;get the adress of read_buff
+mov si, dx          ;si read_buff adresas
 call write_file
-jmp reading         ;after checking 10 or less symbols, repeat
+jmp reading         
 
 get_help:
 mov ah, 9
-mov dx, offset sos  ;save our souls :D
+mov dx, offset sos  
 int 21h
+jmp finish
+
+close_files:
+mov ah, 3Eh   
+mov bx, input_descriptor
+int 21h
+mov ah, 3Eh   
+mov bx, output_descriptor
+int 21h 
 jmp finish
 
 error_open:
 mov ah, 9
-mov dx, offset open_wrong    ;for example 1st file duom.txt doesn't exist
+mov dx, offset open_wrong    
 int 21h 
 jmp close_files
 error_read:
@@ -94,58 +96,43 @@ mov ah, 9
 mov dx, offset write_wrong    
 int 21h 
 
-close_files:
-mov ah, 3Eh   ;close 1st file
-mov bx, input_descriptor
-int 21h
-mov ah, 3Eh   ;close 2nd file
-mov bx, output_descriptor
-int 21h 
-finish:  
-mov ax, 4Ch	  ;yep i wrote the finishing function in the middle :)	
-int 21h
-
-proc duom_name  
+proc file_name  
 duom_file: 
-cmp byte ptr es:[bx], 13  ;if its enter we go back
-je go_back
-cmp byte ptr es:[bx], 20h ;when its a space, that means afterwards comes rez.txt so we end this
-je go_back
+cmp byte ptr es:[bx], 13   ;bx prasideda nuo 82h
+je get_help
+cmp byte ptr es:[bx], 20h 
+je rez_file
 mov dl, byte ptr es:[bx] 
-mov [si], dl  ;fill in myfile 1 by 1 untill we get the whole name
+mov [si], dl               ;si - myfile adresas
 inc bx
 inc si
 jmp duom_file 
+rez_file:
+inc bx     
+cmp byte ptr es:[bx], 13 
+je go_back
+mov dl, byte ptr es:[bx] 
+mov [di], dl               ;di results
+inc di
+jmp rez_file
 go_back:
 ret 
-endp duom_name
+endp file_name
 
-proc rez_name 
-rez_file:
-cmp byte ptr es:[bx], 13 
-je get_back
-mov dl, byte ptr es:[bx] 
-mov [si], dl  
-inc bx
-inc si
-jmp rez_file 
-get_back:
-ret 
-endp rez_name
 
 proc open_file
-mov ah, 3Dh    ;it means we will open a file 
-mov al, 0      ;0 means it's just for reading the file and nothing else
-mov dx, offset myfile ;dx is standard to use of reading
+mov ah, 3Dh     
+mov al, 0      
+mov dx, offset myfile 
 int 21h 
-jc error_open     ;jump if carry flag is 1 (unsuccessful file opening??)
-mov input_descriptor, ax ;so we know which file this is exactly
+jc error_open    
+mov input_descriptor, ax 
 ret  
 endp open_file 
 
 proc create_file
-mov ah, 3Ch    ;create a new file for results
-mov cx, 0      ;0 so it's only for reading
+mov ah, 3Ch   
+mov cx, 0      
 mov dx, offset results
 int 21h 
 jc error_create
@@ -155,35 +142,35 @@ endp create_file
 
 proc write_file 
 cycle:  
-jc error_write   ;unable to write anything
+jc error_write   
 cmp byte ptr[si], '0'   
 jne one 
 mov ah, 40h
 mov bx, output_descriptor
-mov cx, 5  ;lenght of characters to print
+mov cx, 5  
 mov dx, offset nr_zero
 int  21h 
-jmp middle   
+jmp testing   
 
 one:
 cmp byte ptr[si], '1'
 jne two  
 mov ah, 40h
 mov bx, output_descriptor
-mov cx, 6  ;lenght of characters to print
+mov cx, 6  
 mov dx, offset nr_one
 int  21h      
-jmp middle
+jmp testing
 
 two:
 cmp byte ptr[si], '2'
 jne three   
 mov ah, 40h
 mov bx, output_descriptor
-mov cx, 2  ;lenght of characters to print
+mov cx, 2  
 mov dx, offset nr_two
 int  21h  
-jmp middle 
+jmp testing 
 
 three:
 cmp byte ptr[si], '3' 
@@ -193,9 +180,9 @@ mov bx, output_descriptor
 mov cx, 4  
 mov dx, offset nr_three
 int  21h 
-jmp middle 
+jmp testing 
 
-continue2: ;continue1 and continue2 is necessary, otherwise the jumps are out of range
+continue2:
 jmp cycle
 
 four:
@@ -206,7 +193,7 @@ mov bx, output_descriptor
 mov cx, 6 
 mov dx, offset nr_four
 int  21h  
-jmp middle  
+jmp testing  
 
 five:
 cmp byte ptr[si], '5'
@@ -216,7 +203,6 @@ mov bx, output_descriptor
 mov cx, 5  
 mov dx, offset nr_five
 int  21h    
-middle:
 jmp testing    
 
 six:                 
@@ -240,7 +226,7 @@ int  21h
 jmp testing 
 
 continue1:  
-jmp continue2  ;otherwise its too big of a jump
+jmp continue2 
 
 eight:
 cmp byte ptr[si], '8'
@@ -263,7 +249,7 @@ int  21h
 jmp testing
 
 end_of_line:
-cmp byte ptr[si], 13   ;0Dh after 13 always comes 10 as end of line and newline kind of thing so we do newline and ignore 10 later
+cmp byte ptr[si], 13      ;end of line
 jne non_number
 mov ah, 40h
 mov bx, output_descriptor
@@ -273,10 +259,10 @@ int  21h
 jmp testing 
 
 non_number:
-cmp byte ptr[si], 10   ;ignore 10 because its a combo of 13 and 10 meaning newline
+cmp byte ptr[si], 10    
 je testing  
 mov ax, [si]  
-mov element, ax        ;save the non-number symbol in element so we can use it later to offset
+mov element, ax   
 mov ah, 40h
 mov bx, output_descriptor
 mov cx, 1  
@@ -284,11 +270,15 @@ mov dx, offset element
 int  21h  
 
 testing:
-inc si            ;increase position
-dec symbols       ;decrease the amount of symbols left to analyze
+inc si   
+dec symbols  
 mov cx, symbols
 cmp cx, 0
 jne continue1 
 ret   
-endp write_file			
-end strt    
+endp write_file	
+
+finish:  
+mov ax, 4Ch	  
+int 21h		
+END    
